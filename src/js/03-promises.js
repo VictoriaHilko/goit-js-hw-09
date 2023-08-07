0a6c8bd1b2088250fc19eca19881571e07d3792f
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2023-08-07 22:37:37 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
+import { Notify } from "notiflix";
+
+const refs = {
+  form: document.querySelector('.form')
+};
+
+refs.form.addEventListener('submit', onCreatePromise);
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({position, delay});
+    } else {
+      reject({position, delay});
+    }
+    }, delay);
+  });
+}
+
+
+function onCreatePromise(event) {
+  event.preventDefault();
+  // console.log(event.currentTarget);
+
+  const formData = new FormData(event.currentTarget);
+  const dataParams = {};
+
+
+  for (const [key, value] of formData.entries()) {
+    dataParams[key] = Number(value);
+  }
+
+  let { amount, step, delay } = dataParams;
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay += step;
+    createPromise(i, delay)
+    .then(onSuccess)
+    .catch(onError);
+
+    refs.form.reset();
   }
 }
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
+}
+
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
+}
+
